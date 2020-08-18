28eb159ce87c1e48701044a92d0344786b3abdba
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-08-18 12:53:07 +0800

    Test javascript call to api endpoint

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,13 @@
-console.log("Loaded script.js")
+$('#categories').on('click', ".category-0", async function() {
+    console.log(this.id)
+    let response = await axios.get('http://localhost:8080/api/categories')
+    // console.log(response.data)
+    for (let category of response.data.categories) {
+        if (category.category===this.id) {
+            // console.log(category.sub_categories)
+            for (let sub_category of category.sub_categories) {
+                console.log(sub_category.category)
+            }
+        }
+    }
+})
