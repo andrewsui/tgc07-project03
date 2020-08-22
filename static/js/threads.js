b6b3c56ed55975935c6928714368e30631f6cf89
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-08-22 19:01:23 +0800

    Add placeholder code to add or remove classes to html voting elements

--- static/js/threads.js
+++ static/js/threads.js
@@ -1,22 +1,26 @@
-async function vote(threadId, upOrDown) {
-    let url = "/api/threads/" + threadId + "/vote-" + upOrDown;
-    console.log(url)
-    await axios.patch(url);
-}
-
 let voteUpElements = document.querySelectorAll(".vote-up");
 let voteDownElements = document.querySelectorAll(".vote-down");
 
+async function vote(threadId, upOrDown) {
+    let url = "/api/threads/" + threadId + "/vote-" + upOrDown;
+    // console.log(url);
+    await axios.patch(url);
+}
+
 for (let element of voteUpElements) {
-    element.addEventListener("click", ()=> {
+    element.addEventListener("click", () => {
       let threadId = (element.id.replace(/^up-+/i, ''));
-      vote(threadId, "up");
+    //   vote(threadId, "up");
+    //   element.classList.remove("fa-thumbs-o-up");
+    //   element.classList.add("fa-thumbs-up");
     })
   }
 
   for (let element of voteDownElements) {
-    element.addEventListener("click", ()=> {
+    element.addEventListener("click", () => {
       let threadId = (element.id.replace(/^down-+/i, ''));
-      vote(threadId, "down");
+    //   vote(threadId, "down");
+    //   element.classList.remove("fa-thumbs-o-down");
+    //   element.classList.add("fa-thumbs-down");
     })
   }
