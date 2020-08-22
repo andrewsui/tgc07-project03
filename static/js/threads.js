1ee911d0a58d2ad08345697df89d03ccc941bfcd
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-08-22 21:21:45 +0800

    Up and down voting enabled via JavaScript

--- static/js/threads.js
+++ static/js/threads.js
@@ -13,20 +13,33 @@ async function voteCheck(threadId, upOrDown) {
   response.data.response ? console.log(true) : console.log(false)
 }
 
+async function voteRemove(threadId, upOrDown) {
+  let url = "/api/threads/" + threadId + "/vote-" + upOrDown + "-remove";
+  await axios.get(url);
+}
+
 for (let element of voteUpElements) {
     element.addEventListener("click", () => {
       let threadId = (element.id.replace(/^up-+/i, ''));
-    //   vote(threadId, "up");
-    //   element.classList.remove("fa-thumbs-o-up");
-    //   element.classList.add("fa-thumbs-up");
+      vote(threadId, "up");
+      element.classList.remove("fa-thumbs-o-up");
+      element.classList.add("fa-thumbs-up");
+      voteRemove(threadId, "down");
+      let thumbsDownBtn = document.querySelector("#down-"+threadId);
+      thumbsDownBtn.classList.remove("fa-thumbs-down");
+      thumbsDownBtn.classList.add("fa-thumbs-o-down");
     })
   }
 
 for (let element of voteDownElements) {
   element.addEventListener("click", () => {
     let threadId = (element.id.replace(/^down-+/i, ''));
-  //   vote(threadId, "down");
-  //   element.classList.remove("fa-thumbs-o-down");
-  //   element.classList.add("fa-thumbs-down");
+    vote(threadId, "down");
+    element.classList.remove("fa-thumbs-o-down");
+    element.classList.add("fa-thumbs-down");
+    voteRemove(threadId, "up");
+    let thumbsUpBtn = document.querySelector("#up-"+threadId);
+    thumbsUpBtn.classList.remove("fa-thumbs-up");
+    thumbsUpBtn.classList.add("fa-thumbs-o-up");
   })
 }
