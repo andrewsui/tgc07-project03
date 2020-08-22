e10fb02abdde3f29a4eb9a86f684a81385743aef
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-08-22 21:08:00 +0800

    Format document's code

--- static/js/threads.js
+++ static/js/threads.js
@@ -2,9 +2,15 @@ let voteUpElements = document.querySelectorAll(".vote-up");
 let voteDownElements = document.querySelectorAll(".vote-down");
 
 async function vote(threadId, upOrDown) {
-    let url = "/api/threads/" + threadId + "/vote-" + upOrDown;
-    // console.log(url);
-    await axios.patch(url);
+  let url = "/api/threads/" + threadId + "/vote-" + upOrDown;
+  await axios.patch(url);
+}
+
+async function voteCheck(threadId, upOrDown) {
+  let url = "/api/threads/" + threadId + "/vote-" + upOrDown + "-check";
+  let response = await axios.get(url);
+  // console.log(response.data.response);
+  response.data.response ? console.log(true) : console.log(false)
 }
 
 for (let element of voteUpElements) {
@@ -16,11 +22,11 @@ for (let element of voteUpElements) {
     })
   }
 
-  for (let element of voteDownElements) {
-    element.addEventListener("click", () => {
-      let threadId = (element.id.replace(/^down-+/i, ''));
-    //   vote(threadId, "down");
-    //   element.classList.remove("fa-thumbs-o-down");
-    //   element.classList.add("fa-thumbs-down");
-    })
-  }
+for (let element of voteDownElements) {
+  element.addEventListener("click", () => {
+    let threadId = (element.id.replace(/^down-+/i, ''));
+  //   vote(threadId, "down");
+  //   element.classList.remove("fa-thumbs-o-down");
+  //   element.classList.add("fa-thumbs-down");
+  })
+}
