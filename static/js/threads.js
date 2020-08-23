73cbe4d8e418838f21007c3c4d1cc7215169c58e
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-08-23 14:57:17 +0800

    Clicking already selected vote button removes current logged in user's vote

--- static/js/threads.js
+++ static/js/threads.js
@@ -9,44 +9,51 @@ async function vote(threadId, upOrDown) {
 async function voteRemove(threadId, upOrDown) {
   let url = "/api/threads/" + threadId + "/vote-" + upOrDown + "-remove";
   await axios.patch(url);
+  let thumbsBtn = document.querySelector("#" + upOrDown + "-" + threadId);
+  thumbsBtn.classList.remove("fa-thumbs-" + upOrDown );
+  thumbsBtn.classList.add("fa-thumbs-o-" + upOrDown );
 }
 
 window.addEventListener('load', async (event) => {
   for (let element of voteUpElements) {
     let threadId = (element.id.replace(/^up-+/i, ''));
-    let url = "/api/threads/" + threadId + "/vote-" + "up" + "-check";
-    let response = await axios.get(url);
+    let voteCheckUrl = "/api/threads/" + threadId + "/vote-" + "up" + "-check";
+    let response = await axios.get(voteCheckUrl);
     if (response.data.response) {
       element.classList.remove("fa-thumbs-o-up");
       element.classList.add("fa-thumbs-up");
-    }
-    element.addEventListener("click", () => {
-      vote(threadId, "up");
-      element.classList.remove("fa-thumbs-o-up");
-      element.classList.add("fa-thumbs-up");
-      voteRemove(threadId, "down");
-      let thumbsDownBtn = document.querySelector("#down-"+threadId);
-      thumbsDownBtn.classList.remove("fa-thumbs-down");
-      thumbsDownBtn.classList.add("fa-thumbs-o-down");
-    })
+    };
+    element.addEventListener("click", async () => {
+      let response = await axios.get(voteCheckUrl);
+      if (response.data.response) {
+        voteRemove(threadId, "up");
+      } else {
+        vote(threadId, "up");
+        element.classList.remove("fa-thumbs-o-up");
+        element.classList.add("fa-thumbs-up");
+        voteRemove(threadId, "down");
+      }
+    });
   }
   for (let element of voteDownElements) {
     let threadId = (element.id.replace(/^down-+/i, ''));
-    let url = "/api/threads/" + threadId + "/vote-" + "down" + "-check";
-    let response = await axios.get(url);
+    let voteCheckUrl = "/api/threads/" + threadId + "/vote-" + "down" + "-check";
+    let response = await axios.get(voteCheckUrl);
     if (response.data.response) {
       element.classList.remove("fa-thumbs-o-down");
       element.classList.add("fa-thumbs-down");
-    }
-    element.addEventListener("click", () => {
-      vote(threadId, "down");
-      element.classList.remove("fa-thumbs-o-down");
-      element.classList.add("fa-thumbs-down");
-      voteRemove(threadId, "up");
-      let thumbsUpBtn = document.querySelector("#up-"+threadId);
-      thumbsUpBtn.classList.remove("fa-thumbs-up");
-      thumbsUpBtn.classList.add("fa-thumbs-o-up");
-    })
+    };
+    element.addEventListener("click", async () => {
+      let response = await axios.get(voteCheckUrl);
+      if (response.data.response) {
+        voteRemove(threadId, "down");
+      } else {
+        vote(threadId, "down");
+        element.classList.remove("fa-thumbs-o-down");
+        element.classList.add("fa-thumbs-down");
+        voteRemove(threadId, "up");
+      }
+    });
   }
 });
 
