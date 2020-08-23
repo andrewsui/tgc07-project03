8396a75b6acf1f4c2d9fa06e52a7e280e629846d
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-08-23 14:14:40 +0800

    Update thread now auto-populates previous value for sub-category via JavaScript

--- static/js/sub-categories.js
+++ static/js/sub-categories.js
@@ -1,13 +1,12 @@
-$("#categories").on("click", ".category-0", async function() {
-    let subCategoriesUrl = "/api/sub-categories/" + this.id;
+async function displaySubCategories(subCategoriesUrl) {
     let response = await axios.get(subCategoriesUrl);
     let parentElement = document.querySelector("#sub_categories");
     parentElement.innerText = "";
-    console.log(response.data.results[0].sub_categories);
+    // console.log(response.data.results[0].sub_categories);
     for (let subCategory of response.data.results[0].sub_categories) {
-        console.log(subCategory.category)
-        console.log(subCategory._id.$oid)
-        console.log(this.value)
+        // console.log(subCategory.category)
+        // console.log(subCategory._id.$oid)
+        // console.log(this.value)
         let newElement = document.createElement("option");
         newElement.value = subCategory._id.$oid;
         newElement.id = subCategory._id.$oid;
@@ -15,4 +14,47 @@ $("#categories").on("click", ".category-0", async function() {
         newElement.innerText = subCategory.category;
         parentElement.appendChild(newElement);
     }
+}
+
+$("#categories").on("click", ".category-0", async function() {
+    let subCategoriesUrl = "/api/sub-categories/" + this.id;
+    await displaySubCategories(subCategoriesUrl);
+    // let response = await axios.get(subCategoriesUrl);
+    // let parentElement = document.querySelector("#sub_categories");
+    // parentElement.innerText = "";
+    // console.log(response.data.results[0].sub_categories);
+    // for (let subCategory of response.data.results[0].sub_categories) {
+    //     console.log(subCategory.category)
+    //     console.log(subCategory._id.$oid)
+    //     console.log(this.value)
+    //     let newElement = document.createElement("option");
+    //     newElement.value = subCategory._id.$oid;
+    //     newElement.id = subCategory._id.$oid;
+    //     newElement.className = "category-1";
+    //     newElement.innerText = subCategory.category;
+    //     parentElement.appendChild(newElement);
+    // }
+})
+
+window.addEventListener('load', async (event) => {
+    let subCategoriesElement = document.querySelector("#categories");
+    let subCategoriesUrl = "/api/sub-categories/" + subCategoriesElement.value;
+    await displaySubCategories(subCategoriesUrl);
+    let previousSubCategoryId = document.querySelector("#previous_sub_categories").value;
+    previousSubCategoryId ? document.getElementById(previousSubCategoryId).selected = true : null;
+    // let response = await axios.get(subCategoriesUrl);
+    // let parentElement = document.querySelector("#sub_categories");
+    // parentElement.innerText = "";
+    // console.log(response.data.results[0].sub_categories);
+    // for (let subCategory of response.data.results[0].sub_categories) {
+    //     console.log(subCategory.category)
+    //     console.log(subCategory._id.$oid)
+    //     console.log(this.value)
+    //     let newElement = document.createElement("option");
+    //     newElement.value = subCategory._id.$oid;
+    //     newElement.id = subCategory._id.$oid;
+    //     newElement.className = "category-1";
+    //     newElement.innerText = subCategory.category;
+    //     parentElement.appendChild(newElement);
+    // }
 })
