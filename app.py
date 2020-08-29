029e7221a7dfa9059d19638978d1ec607d25bb16
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-08-29 18:31:54 +0800

    Add basic post comment form at bottom of review thread page

--- app.py
+++ app.py
@@ -208,11 +208,9 @@ def display_thread(thread_id):
     if request.method == 'GET':
         thread = m_services.threads_get_one(db, thread_id)
         return render_template('threads/single-thread.html', thread=thread)
-        # RETURN TO BE UPDATED
-        # return {
-        #     'results': json.loads(dumps(thread))
-        # }
-    # ADD POST REQUEST TO CREATE NEW COMMENTS
+    elif request.method == 'POST':
+        m_services.comments_create(db, request.form, thread_id)
+        return request.form
 
 @app.route('/threads/create', methods=['GET','POST'])
 @flask_login.login_required
